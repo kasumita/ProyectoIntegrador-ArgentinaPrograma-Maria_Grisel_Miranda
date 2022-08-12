9e02cd90920a96bf917cd2eeff03226943e823d0
kasumita <kasumita@gmail.com>
2022-08-12 00:40:24 -0300

    agregada imagen de banner

--- ProyectoIntegrador/src/app/app.module.ts
+++ ProyectoIntegrador/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { LogoAPComponent } from './componentes/logo-ap/logo-ap.component';
+import { SocialComponent } from './componentes/social/social.component';
+import { BannerComponent } from './componentes/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
