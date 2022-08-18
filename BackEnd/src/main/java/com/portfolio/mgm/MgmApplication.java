f96beb5ef10de43b338214233f9e812080f5e771
kasumita <kasumita@gmail.com>
2022-08-17 21:39:11 -0300

    creando carpeta backend

---
+++ BackEnd/src/main/java/com/portfolio/mgm/MgmApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.mgm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgmApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/portfolio/mgm/MgmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.mgm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
