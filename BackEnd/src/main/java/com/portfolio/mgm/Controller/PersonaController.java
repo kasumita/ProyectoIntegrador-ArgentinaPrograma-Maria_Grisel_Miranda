0e39c8160f9889b6f581c50b216799142f92fbea
kasumita <kasumita@gmail.com>
2022-10-20 00:46:33 -0300

    .

--- BackEnd/src/main/java/com/portfolio/mgm/Controller/PersonaController.java
+++ BackEnd/src/main/java/com/portfolio/mgm/Controller/PersonaController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontend-77546.web.app")
 @RestController
 
 
--- FrontEnd/src/app/service/persona.service.ts
+++ FrontEnd/src/app/service/persona.service.ts
@@ -8,7 +8,7 @@ import { persona } from '../model/persona.model';
   providedIn: 'root'
 })
 export class PersonaService {
-URL = 'http://localhost:8080/personas/';
+URL = 'https://backendprueba22.herokuapp.com/personas/';
   constructor(private http: HttpClient) { }
 
   public getPersona(): Observable<persona> {
